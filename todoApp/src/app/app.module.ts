3dc692465b8034942bd315db55b11c2d1ea3bec5
salaheddineobt <salaheddine.ouladbentaib@gmail.com>
2022-06-23 18:27:08 +0100

    save

--- todoApp/src/app/app.module.ts
+++ todoApp/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoPipe } from './todo.pipe';
+import { TodoComponent } from './todo/todo.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoPipe,
+    TodoComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
